bf4a5f1bfd28c6595e78c2f80c130f41992da1b2
RyanKor <equuse3144@gmail.com>
2020-02-20 11:41:18 +0700

    studying ref parts

---
+++ validation/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './ValidationSample'
+import ValidationSample from './ValidationSample'
+function App() {
+  return (
+<ValidationSample></ValidationSample>
+  );
+}
+
+export default App;
