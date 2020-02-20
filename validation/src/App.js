b6c995b0d9a94866d447966f099d748c6979886c
RyanKor <equuse3144@gmail.com>
2020-02-20 14:02:14 +0700

    making scrollbox with React

--- validation/src/App.js
+++ validation/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './ValidationSample'
-import ValidationSample from './ValidationSample'
-function App() {
-  return (
-<ValidationSample></ValidationSample>
-  );
-}
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./ValidationSample";
+import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
 
+//코드를 함수형으로 작성하느냐, 클래스형으로 작성하느냐?
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={ref => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨밑으로
+        </button>
+      </div>
+    );
+  }
+}
 export default App;
