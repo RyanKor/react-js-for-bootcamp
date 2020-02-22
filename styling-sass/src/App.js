680b8e3e7a482019c408a8b304de66d273a208da
RyanKor <equuse3144@gmail.com>
2020-02-22 07:57:10 +0700

    first sass

---
+++ styling-sass/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import SassComponent from "./SassComponent.js";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
