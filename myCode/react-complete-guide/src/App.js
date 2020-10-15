12d1f6c4eb2b793931f029c05072243518639d72
RyanKor <equuse3144@gmail.com>
2020-10-15 22:32:21 +0700

    class component basic added

--- myCode/react-complete-guide/src/App.js
+++ myCode/react-complete-guide/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import Person from './Person/Person'
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    persons : [
+      {name:"Ryan", age:"27"},
+      {name:"Ryu", age:"22"},
+      {name:"Rz", age:"25"},
+    ]
+  }
+  switchNameHandler = ()=>{
+    console.log("Was Clicked")
+    // Don't do this : this.state.persons[0].name = "SeungTae Kim"
+    this.setState({    persons : [
+      {name:"seungtae Kim", age:"27"},
+      {name:"Ryu", age:"22"},
+      {name:"Rz", age:"25"},
+    ]})
+  }
+  render(){return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, I'm React App</h1>
+      <p>This is really working</p>
+      <button onClick={this.switchNameHandler}>Switching Button</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} >A new Person</Person> 
+      
     </div>
   );
 }
-
+}
+//<Person name="Rz" age="25" >A new Person</Person> 에서 태그 사이에 들어가있는 값이 props.children이다.
 export default App;
--- myCode/react-complete-guide/src/Person/Person.js
+++ myCode/react-complete-guide/src/Person/Person.js
@@ -1,3 +1,12 @@
-function person(){
-    
+import React from 'react';
+
+
+const person = (props)=>{
+    console.log(props)
+return (<div><h1>I'm a {props.name}. My age is {props.age}</h1>
+<h1>{props.children}</h1>
+    </div>
+)
 }
+
+export default person
