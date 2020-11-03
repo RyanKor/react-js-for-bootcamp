f5cbf71fa5e247ed6cbd493a174f298e8cec1ebe
RyanKor <equuse3144@gmail.com>
2020-11-03 20:52:23 +0700

    customer.js / app.js refactoring test

--- myCode/management/src/App.js
+++ myCode/management/src/App.js
@@ -2,21 +2,62 @@ import './App.css';
 import Customer from './components/Customer'
 
 
-const customer = {
+const customer = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
   'name' : 'ryan kim',
   'age' : '27',
   'gender' : 'man',
   'job' : 'software engineer'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name' : '깽나니우스',
+  'age' : '51',
+  'gender' : 'woman',
+  'job' : '가위손'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name' : '로버트 깽나니우스 주니어',
+  'age' : '25',
+  'gender' : 'woman',
+  'job' : '금융인'
 }
+]
 
 function App() {
   return (
-    <Customer
-    name={customer.name}
-    age={customer.age}
-    gender={customer.gender}
-    job={customer.job}
-    />
+
+    // <Customer
+    // id={customer[0].id}
+    // image={customer[0].image}
+    // name={customer[0].name}
+    // age={customer[0].age}
+    // gender={customer[0].gender}
+    // job={customer[0].job}
+    // />
+
+    <div>
+    {
+      //map 함수는 React의 불변성을 유지해주는 유용한 JS 메소드다.
+      //다만, map을 활용할 땐, 반드시 key라는 속성을 넣어줘야한다.
+      customer.map(element=>
+        <Customer
+        key={element.id} // key는 각 컴포넌트를 구분해주는 역할을 해준다.
+        id={element.id}
+        image={element.image}
+        name={element.name}
+        age={element.age}
+        gender={element.gender}
+        job={element.job}
+        />
+      )
+    }
+        </div>
   );
 }
 
--- myCode/management/src/components/Customer.js
+++ myCode/management/src/components/Customer.js
@@ -6,11 +6,32 @@ class Customer extends React.Component{
     render(){ 
        return( 
         <div>
-            <h2>{this.props.name}</h2>
+            <CustomerProfile id={this.props.id} image={this.props.image} name = {this.props.name} />
+            <CustomerInfo age={this.props.age} gender={this.props.gender} job={this.props.job} />
+        </div>
+        )
+    }
+}
+
+class CustomerProfile extends React.Component{
+    render(){
+        return(
+            <div>
+                <img src={this.props.image} alt="profile"/>
+        <h2>{this.props.name} ({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+
+class CustomerInfo extends React.Component{
+    render(){
+        return (
+            <div>
             <p>{this.props.age}</p>
             <p>{this.props.gender}</p>
             <p>{this.props.job}</p>
-        </div>
+            </div>
         )
     }
 }
