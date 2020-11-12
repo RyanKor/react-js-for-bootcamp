b306aac3446ff71e760a7d1ea2bdc0c789fd5201
RyanKor <equuse3144@gmail.com>
2020-11-12 21:53:57 +0700

    add customer api

--- myCode/management/client/src/App.js
+++ myCode/management/client/src/App.js
@@ -8,6 +8,7 @@ import TableBody from '@material-ui/core/TableRow'
 import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell'
 import {withStyles} from '@material-ui/core/styles'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 
 const styles = themes =>({
@@ -18,20 +19,29 @@ const styles = themes =>({
   },
   table:{
     minWidth: 1080
+  },
+  progress: {
+    margin: themes.spacing.unit * 2
   }
 })
 
 class App extends Component {
   state = {
-    customers: ""
+    customers: "",
+    completed: 0 //progress bar setting
   }
   componentDidMount(){
+    this.timer = setInterval(this.progress, 20)
     this.callApi().then(res =>this.setState({customers:res})).catch(err=>console.log(err))
   }
   callApi = async()=>{
     const response = await fetch('/api/customers')
     const body = await response.json()
     return body
+  }
+  progress = ()=>{
+    const {completed} = this.state
+    this.setState({completed: completed >= 100 ? 0 : completed + 1})
   }
   render(){  
     const {classes} = this.props;
@@ -66,6 +76,11 @@ class App extends Component {
           }
           ) : ""
         }
+        <TableRow>
+          <TableCell colSpan='6' align='center'>
+            <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+          </TableCell>
+        </TableRow>
         </TableBody>
       </Table>
         </Paper>
