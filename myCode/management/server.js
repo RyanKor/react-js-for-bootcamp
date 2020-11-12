c791907e8f71b10d6c5f2ccaed3cdd404406a5da
RyanKor <equuse3144@gmail.com>
2020-11-12 09:26:23 +0700

    front-back end http connection

--- myCode/management/server.js
+++ myCode/management/server.js
@@ -7,8 +7,33 @@ const port = process.env.PORT || 5000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/api/hello', (req,res)=>{
-    res.send({message: "Hello Express!"})
+app.get('/api/customers', (req,res)=>{
+    res.send([
+        {
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name' : 'ryan kim',
+        'age' : '27',
+        'gender' : 'man',
+        'job' : 'software engineer'
+      },
+      {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/2',
+        'name' : '깽나니우스',
+        'age' : '51',
+        'gender' : 'woman',
+        'job' : '가위손'
+      },
+      {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/3',
+        'name' : '로버트 깽나니우스 주니어',
+        'age' : '25',
+        'gender' : 'woman',
+        'job' : '금융인'
+      }
+      ])
 })
 
 
