3f157fecab62078dac827143bf0282935637f225
RyanKor <equuse3144@gmail.com>
2020-11-16 07:30:36 +0700

    udemy max material codes, velopert material codes deleted / news-api viewr codes writing now

---
+++ myCode/news-viewer/src/App.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+const App =  ()=>{
+  const [data, setData] = useState(null)
+  const onClick = async ()=>{
+    try{
+    const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=')
+    setData(response.data)
+  }catch(e){
+    console.log(e)
+  }
+  }
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>Loading</button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}      
+    </div>
+  )
+}
+
+export default App
